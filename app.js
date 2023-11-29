1a403edfc47cccac86e4c20ddf9211dbf4d644a5
A-li-ce <aliceraj3012@gmail.com>
2023-11-29 18:32:11 +0530

    update

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.set("views", path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "/Public")));
 app.use(session(sessionOptions))
 app.use(flash());
 
